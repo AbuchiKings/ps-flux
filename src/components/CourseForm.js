cfec45165f41d11d0ec6109acdba834274ada42f
Abuchi Kingsley <abuchikingsley76@gmail.com>
2020-02-04 22:55:05 +0100

    Final stage prior to addition of flux

---
+++ src/components/CourseForm.js
@@ -0,0 +1,58 @@
+import React from "react";
+import TextInput from './common/TextInput';
+import PropTypes from "prop-types";
+
+function CourseForm({ course, onTitleChange, onSubmit, errors }) {
+    return (
+        <form onSubmit={onSubmit}>
+            <TextInput
+                id="title"
+                label="Title"
+                name="title"
+                value={course.title}
+                onChange={onTitleChange}
+                error={errors.title}
+            />
+
+            <div className="form-group">
+                <label htmlFor="author">Author</label>
+                <div className="field">
+                    <select
+                        id="author"
+                        name="authorId"
+                        onChange={onTitleChange}
+                        value={course.authorId || ""}
+                        className="form-control"
+                    >
+                        <option value="" />
+                        <option value="1">Cory House</option>
+                        <option value="2">Scott Allen</option>
+                    </select>
+                </div>
+                {errors.authorId && (<div className="alert alert-danger">{errors.authorId}</div>)}
+            </div>
+
+
+            <TextInput
+                id="category"
+                label="Category"
+                name="category"
+                onChange={onTitleChange}
+                value={course.category}
+                error={errors.category}
+            />
+
+
+            <input type="submit" value="Save" className="btn btn-primary" />
+        </form>
+    );
+}
+
+CourseForm.propTypes = {
+    course: PropTypes.object.isRequired,
+    onSubmit: PropTypes.func.isRequired,
+    onChange: PropTypes.func.isRequired,
+    errors: PropTypes.object.isRequired
+  };
+
+export default CourseForm;
---
+++ src/components/CoursesPage.js
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from 'react';
+import { getCourses } from "../api/courseApi";
+import CourseList from './CourseList';
+import { Link } from 'react-router-dom'
+
+function CoursesPage() {
+
+    const [courses, setCourses] = useState([]);
+
+    useEffect(() => {
+        getCourses().then(_courses => {
+            setCourses(_courses);
+        });
+    }, [])
+
+
+    return (
+        <>
+            <h2>Courses</h2>
+            <Link className="btn btn-primary" to="/course"> Add Course </Link>
+            <CourseList courses={courses} />
+        </>
+    )
+
+}
+
+
+export default CoursesPage;
+
+// class CoursesPage extends React.Component {
+//     state = {
+//         courses: []
+//     };
+
+//     componentDidMount() {
+//         getCourses().then(courses => {
+//             this.setState({ courses: courses });
+//         });
+//     }
+
+//     render() {
+//         return (
+//             <>
+//                 <h2>Courses</h2>
+//                 <table className="table">
+//                     <thead>
+//                         <tr>
+//                             <th>Title</th>
+//                             <th>Author</th>
+//                             <th>Category</th>
+//                         </tr>
+//                     </thead>
+//                     <tbody>
+//                         {
+//                             this.state.courses.map(course => {
+//                                 return (
+//                                     <tr key={course.id}>
+//                                         <td>{course.title}</td>
+//                                         <td>{course.authorId}</td>
+//                                         <td>{course.category}</td>
+//                                     </tr>
+//                                 )
+//                             })
+//                         }
+//                     </tbody>
+//                 </table>
+//             </>
+//         )
+//     }
+// }
